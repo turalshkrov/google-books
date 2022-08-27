ba025433850fb13e95d54990b857ad801c2804f5
Tural Şükürov <tural.shkrov@gmail.com>
2022-08-27 11:50:08 +0400

    <show-book>

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const searchButton = document.querySelector('#search-button');
 const searchForm = document.querySelector('#search-form');
 const searchBooks = document.querySelector('#search-books');
 const recomendedBooks = document.querySelector('#book-for-you');
+const mainSection = document.querySelector('main');
 
+let booksArray = [];
 let searchBooksArray = [];
+let recomendedBooksArray = [];
 let recomendedBooksIds = ['nVGKDgAAQBAJ', 'gFbiTgEACAAJ', '9OUCDQAAQBAJ', '5vbJtQEACAAJ', '9ooz3WjkueYC'];
 
 const url = ('https://www.googleapis.com/books/v1/volumes');
@@ -13,13 +16,13 @@ async function getReacomendedBooks(id) {
     await fetch(url + '/' + id)
     .then(data => data.json())
     .then(book => {
-        console.log(book);
+        booksArray.push(book);
+        recomendedBooksArray.push(book)
         recomendedBooks.innerHTML += `
-        <div class="book">
+        <div class="book" id="${book.id}">
             <img src="${book.volumeInfo.imageLinks.thumbnail}" alt="">
             <div class="book-title">${book.volumeInfo.title}</div>
-        </div>
-        `
+        </div>`
     })
 }
 
@@ -33,16 +36,15 @@ async function getSearchBooks() {
     .then(result => {
         searchBooksArray = result.items;
     })
-    searchBooks.innerHTML = ''
+    searchBooks.innerHTML = '';
     searchBooksArray.map(book => {
-        console.log(book);
         searchBooks.innerHTML += `
-        <div class="book">
-            <img src="${book.volumeInfo.imageLinks.thumbnail}" alt="">
-            <div class="book-title">${book.volumeInfo.title}</div>
-        </div>
-        `
+        <div class="book" id="${book.id}">
+        <img src="${book.volumeInfo.imageLinks.thumbnail}" alt="">
+        <div class="book-title">${book.volumeInfo.title}</div>
+        </div>`
     })
+    booksArray = searchBooksArray.concat(recomendedBooksArray);
 }
 
 searchButton.addEventListener('click', getSearchBooks);
@@ -51,3 +53,36 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
+function showBook(volumeInfo) {
+    mainSection.innerHTML += `
+    <div class='book-more-info'>
+    <div class='book-info'>
+        <img src="${volumeInfo.imageLinks.thumbnail}">
+        <div class='close-book-info'><i class="fa-solid fa-xmark"></i></div>
+        <ul class="book-info-list">
+        <li>Title: ${volumeInfo.title}</li>
+        <li>Author: ${volumeInfo.authors}</li>
+        <li>Published date: ${volumeInfo.publishedDate}</li>
+        <li>Publisher: ${volumeInfo.publisher}</li>
+        <li>Language: ${volumeInfo.language}</li>
+        <li>page: ${volumeInfo.pageCount}</li>
+        </ul>
+        </div>
+    </div>`;
+    console.log(volumeInfo);
+}
+
+mainSection.addEventListener('click', (e) => {
+    if(e.target.parentElement.className === 'book'){
+        let bookElement = e.target.parentElement;
+        booksArray.map(book => {
+            if (book.id === bookElement.id) {
+                showBook(book.volumeInfo);
+            }
+        })
+    }
+    else if(e.target.className === 'close-book-info' || e.target.className === 'fa-solid fa-xmark') {
+        let bookElement = document.querySelector('.book-more-info');
+        bookElement.remove();
+    }
+})
